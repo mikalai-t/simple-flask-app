88e6bcfd5276314a824a76df84c796dea9a69ecc
Nikalai Trafimovich <trafimovich.n@gmail.com>
2020-07-13 00:53:59 +0300

    Use counter to make application like stateful

--- ext/views.py
+++ ext/views.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import socket
 import pprint
 from flask import jsonify, request, Response
@@ -16,4 +17,27 @@ def health_check():
 
 
 def hello(name):
-    return 'Hello, <b>{}</b>, from application, running on <u>{}</u>'.format(name, socket.gethostname())
+    dest = '/tmp/flask-app/session.counter'
+    counter = 0
+    if os.path.isdir(os.path.dirname(dest)):
+        try:
+            with open(dest, 'r') as f:
+                counter = int(f.read().strip())
+        except (IOError, FileNotFoundError):
+            pass
+    else:
+        try:
+            os.makedirs(os.path.dirname(dest), 0o750)
+        except (IOError, PermissionError):
+            pass
+
+    counter += 1
+
+    try:
+        with open(dest, 'w+') as f:
+            f.write(str(counter))
+    except (IOError, FileNotFoundError, PermissionError):
+        pass
+
+    return 'Hello, <b>{}</b>, from application, running on <u>{}</u>. You visited this app <b>{}</b> time(s).'.format(
+        name, socket.gethostname(), counter)
